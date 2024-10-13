088d3b0d0b78a68cbb74ac255bee82a89b62499e
faideksan <faideksan.oniandini@gmail.com>
2024-10-14 01:21:33 +0700

    Memasukan file ke Git Bash lalu ke GitHub

---
+++ telemetry_settings.php
@@ -0,0 +1,20 @@
+<?php
+
+$db_type="mysql"; //Type of db: "mysql", "sqlite" or "postgresql"
+
+// Sqlite3 settings
+$Sqlite_db_file = "../telemetry.sql";
+
+// Mysql settings
+$MySql_username="root";
+$MySql_password="123";
+$MySql_hostname="192.168.1.79";
+$MySql_databasename="speedtest";
+
+// Postgresql settings
+$PostgreSql_username="USERNAME";
+$PostgreSql_password="PASSWORD";
+$PostgreSql_hostname="DB_HOSTNAME";
+$PostgreSql_databasename="DB_NAME";
+
+?>
